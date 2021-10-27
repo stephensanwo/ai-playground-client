d7b31a119ddc581daa3cc1f0abadb14495ff67a0
stephen sanwo <me@stephensanwo.com>
2021-10-28 00:43:39 +0100

    Router

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import "./style.scss";
-import {
-  ChartLineSmooth32,
-  Cognitive32,
-  PhraseSentiment32,
-} from "@carbon/icons-react";
 import { Link } from "react-router-dom";
 import openai from "../../assets/images/svg/openai.svg";
 import ludwig from "../../assets/images/svg/ludwig.svg";
 import prophet from "../../assets/images/svg/prophet.svg";
-import eleutherai from "../../assets/images/svg/eleutherai.svg";
 import Button from "../../components/Button";
-import screen_image from "../../assets/images/svg/screen-image.svg";
 
 const Landing = () => {
   return (
@@ -19,18 +12,17 @@ const Landing = () => {
       <div className="landing-content">
         <div className="landing-description">
           <div className="landing-description-content">
-            <h1>No Code</h1>
-            <h1 style={{ color: "#039874" }}>AI Playground</h1>
+            <h1>Artificial Intelligence</h1>
+            <h1 style={{ color: "#039874" }}>Playground</h1>
             <p>
-              From predictive analytics to natural language understanding,
-              Gallium is a playground for Non-technical users across several
-              functions to explore powerful open source AI models without
-              learning to code.
+              AI Playground is where I test stuff I build with open source and
+              custom Machine Learning and Deep Learning models
             </p>
             <p style={{ marginTop: "2rem" }}>
-              The tools deployed on Gallium are built by Stephen Sanwo, mostly
-              with publicly available open source models. Check out my blog at
-              stephensanwo.dev/blog and follow me on twitter @stephensanwo
+              The tools deployed on AI Playground are built by Stephen Sanwo,
+              mostly with publicly available open source models. Check out my
+              blog at stephensanwo.dev/blog and follow me on twitter
+              @stephensanwo
             </p>
             <div className="landing-navs">
               <Button button_title="Explore" button_link="/navigation" />
