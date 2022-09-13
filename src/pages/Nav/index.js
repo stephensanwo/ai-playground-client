96cea69e3a11bdd95c3d4e531486ac12d95b50d2
Stephen Sanwo <Stephen.sanwo@icloud.com>
2022-09-13 22:10:59 +0100

    Update Nav Page copy

--- src/pages/Nav/index.js
+++ src/pages/Nav/index.js
@@ -15,45 +15,54 @@ const Nav = () => {
           <h2>Welcome, what do you want to do?</h2>
         </div>
         <div className="nav-categories">
-          <Link to="/">
+          <a
+            href="https://www.freecodecamp.org/news/how-to-build-an-ai-chatbot-with-redis-python-and-gpt/"
+            target="blank"
+          >
             <div className="nav-category">
               <div>
-                <h4>Forecasting</h4>
+                <h4>Learn: Build a Fullstack AI Chatbot</h4>
                 <p>
-                  Run forecasts on any data with advanced AI models like
-                  Facebook Prophet and Uber's Ludwig{" "}
+                  The AI Chatbot Handbook – How to Build an AI Chatbot with
+                  Redis, Python, and GPT (FreeCodeCamp)
                 </p>
               </div>
 
               <img src={forecasting} alt="" />
             </div>
-          </Link>
-          <Link to="/">
+          </a>
+          <a
+            href="https://github.com/stephensanwo/fullstack-ai-chatbot"
+            target="blank"
+          >
             <div className="nav-category">
               <div>
-                <h4>GPT-J Chat Bot</h4>
+                <h4>AI Chatbot Project</h4>
                 <p>
-                  Take the GPT-J 6b AI Model for a spin by engaging it in a
-                  conversation
+                  Opensource fullstack conversational AI Chatbot project and
+                  source code (Github)
                 </p>
               </div>
 
               <img src={nlp} alt="" />
             </div>
-          </Link>
-          <Link>
+          </a>
+          <a
+            href="https://hypercompose.com://github.com/stephensanwo/fullstack-ai-chatbot"
+            target="blank"
+          >
             <div className="nav-category">
               <div>
-                <h4>Essay Summarisation</h4>
+                <h4>HYPERCOMPOSE (Alpha)</h4>
                 <p>
-                  Too lazy to read?, Get a summarized version of a long article
-                  or book.
+                  Supercharge your writing with the world's most powerful
+                  Artificial Intelligence
                 </p>
               </div>
 
               <img src={summarize} alt="" />
             </div>
-          </Link>
+          </a>
         </div>
       </div>
     </div>
